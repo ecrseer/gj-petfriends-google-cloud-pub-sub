3f423bb140e609f8cc6caef7f359980c3a8c88e7
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-09-12 01:04:30 -0300

    event domain rules abstract

---
+++ src/main/java/gj/infnet/gjpetfriendsgcp2/eventos/IEventoDominio.java
@@ -0,0 +1,8 @@
+package gj.infnet.gjpetfriendsgcp2.eventos;
+
+import java.util.Date;
+
+public interface IEventoDominio {
+
+    Date quando;
+}
--- src/main/java/gj/infnet/gjpetfriendsgcp2/eventos/TransporteMudouRota.java
+++ src/main/java/gj/infnet/gjpetfriendsgcp2/eventos/TransporteMudouRota.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class TransporteMudouRota {
+public class TransporteMudouRota implements IEventoDominio {
 
     private Long idTransporte;
     private Pedido pedido;
