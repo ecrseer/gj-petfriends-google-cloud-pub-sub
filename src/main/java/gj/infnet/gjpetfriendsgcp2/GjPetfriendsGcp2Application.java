f7a9ce8c1b93ecd7821f284c16018ac63c350a43
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-09-12 00:06:39 -0300

    enable h2 create gcp pub sub

---
+++ src/main/java/gj/infnet/gjpetfriendsgcp2/GjPetfriendsGcp2Application.java
@@ -0,0 +1,14 @@
+package gj.infnet.gjpetfriendsgcp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjPetfriendsGcp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GjPetfriendsGcp2Application.class, args);
+        System.out.println("oladd");
+    }
+
+}
---
+++ src/test/java/gj/infnet/gjpetfriendsgcp2/GjPetfriendsGcp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package gj.infnet.gjpetfriendsgcp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjPetfriendsGcp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
